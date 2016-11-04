fdfa63ebea5a555fe1094aac9a4ffebce44836a9
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2016-11-04 13:59:58 +0100

    Created average pooling

---
+++ bob/learn/tensorflow/layers/AveragePooling.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# @author: Tiago de Freitas Pereira <tiago.pereira@idiap.ch>
+# @date: Wed 11 May 2016 17:38 CEST
+
+import tensorflow as tf
+from bob.learn.tensorflow.util import *
+from .MaxPooling import MaxPooling
+
+
+class AveragePooling(MaxPooling):
+
+    def __init__(self, name, shape=[1, 2, 2, 1],
+                 strides=[1, 1, 1, 1],
+                 batch_norm=False,
+                 activation=None):
+        """
+        Constructor
+        """
+        super(AveragePooling, self).__init__(name, activation=activation, batch_norm=batch_norm)
+        self.shape = shape
+        self.strides = strides
+
+    def get_graph(self, training_phase=True):
+        with tf.name_scope(str(self.name)):
+            output = tf.nn.avg_pool(self.input_layer, ksize=self.shape, strides=self.strides, padding='SAME')
+
+            if self.batch_norm:
+                output = self.batch_normalize(output, training_phase)
+
+            if self.activation is not None:
+                output = self.activation(output)
+
+            return output
