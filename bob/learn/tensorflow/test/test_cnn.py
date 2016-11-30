Fixed test units

bob/learn/tensorflow/test/test_cnn.py
@@ -102,7 +102,8 @@ def test_cnn_trainer():
                       prefetch=False,
                       learning_rate=constant(0.05, name="regular_lr"),
                       optimizer=tf.train.AdamOptimizer(name="adam_softmax"),
-                      temp_dir=directory)
+                      temp_dir=directory
+                      )
 
     trainer.train(train_data_shuffler)
 
@@ -144,7 +145,8 @@ def test_siamesecnn_trainer():
                              analizer=None,
                              learning_rate=constant(0.05, name="siamese_lr"),
                              optimizer=tf.train.AdamOptimizer(name="adam_siamese"),
-                             temp_dir=directory)
+                             temp_dir=directory
+                             )
 
     trainer.train(train_data_shuffler)
 
@@ -187,7 +189,8 @@ def test_tripletcnn_trainer():
                              analizer=None,
                              learning_rate=constant(0.05, name="triplet_lr"),
                              optimizer=tf.train.AdamOptimizer(name="adam_triplet"),
-                             temp_dir=directory)
+                             temp_dir=directory
+                             )
 
     trainer.train(train_data_shuffler)
 

bob/learn/tensorflow/test/test_cnn_pretrained_model.py
@@ -83,8 +83,9 @@ def test_cnn_pretrained():
                       prefetch=False,
                       learning_rate=constant(0.05, name="regular_lr"),
                       optimizer=tf.train.AdamOptimizer(name="adam_pretrained_model"),
-                      temp_dir=directory)
-    import ipdb; ipdb.set_trace();
+                      temp_dir=directory
+                      )
+
     trainer.train(train_data_shuffler)
     accuracy = validate_network(validation_data, validation_labels, scratch)
     assert accuracy > 85
@@ -103,7 +104,8 @@ def test_cnn_pretrained():
                       prefetch=False,
                       learning_rate=None,
                       temp_dir=directory2,
-                      model_from_file=os.path.join(directory, "model.ckp"))
+                      model_from_file=os.path.join(directory, "model.ckp")
+                      )
 
     trainer.train(train_data_shuffler)
 

bob/learn/tensorflow/test/test_dnn.py
@@ -59,7 +59,8 @@ def test_dnn_trainer():
                       prefetch=False,
                       learning_rate=constant(0.05, name="dnn_lr"),
                       optimizer=tf.train.AdamOptimizer(name="adam_dnn"),
-                      temp_dir=directory)
+                      temp_dir=directory
+                      )
     trainer.train(train_data_shuffler)
 
     accuracy = validate_network(validation_data, validation_labels, architecture)