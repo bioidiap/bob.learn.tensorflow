80eb72a7a744e44afefef1c91d29e552d962ed2c
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2016-12-06 10:48:47 +0100

    Reorganize the modules

--- bob/learn/__init__.py
+++ bob/learn/__init__.py
@@ -2,12 +2,3 @@
 from pkgutil import extend_path
 __path__ = extend_path(__path__, __name__)
 
-from bob.learn.tensorflow import analyzers
-from bob.learn.tensorflow import datashuffler
-from bob.learn.tensorflow import initialization
-from bob.learn.tensorflow import layers
-from bob.learn.tensorflow import loss
-from bob.learn.tensorflow import network
-from bob.learn.tensorflow import trainers
-from bob.learn.tensorflow import utils
-
--- bob/learn/tensorflow/__init__.py
+++ bob/learn/tensorflow/__init__.py
@@ -1,2 +1,12 @@
 # gets sphinx autodoc done right - don't remove it
 __all__ = [_ for _ in dir() if not _.startswith('_')]
+
+
+from . import analyzers
+from . import datashuffler
+from . import initialization
+from . import layers
+from . import loss
+from . import network
+from . import trainers
+from . import utils
