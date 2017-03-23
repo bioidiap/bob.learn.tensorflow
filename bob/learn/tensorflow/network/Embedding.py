2c26f7da789834dffe364bc673fd0cbb45ee5bfd
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2017-03-23 10:23:34 +0100

    Introduced the Embeddings

---
+++ bob/learn/tensorflow/network/Embedding.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+# @author: Tiago de Freitas Pereira <tiago.pereira@idiap.ch>
+# @date: Wed 11 May 2016 09:39:36 CEST 
+
+
+import tensorflow as tf
+from bob.learn.tensorflow.utils.session import Session
+
+
+class Embedding(object):
+    """
+    Embedding
+    """
+    def __init__(self, input, graph):
+        self.input = input
+        self.graph = graph
+
+    def __call__(self, data):
+        session = Session.instance().session
+        feed_dict = {self.input: data}
+
+        return session.run([self.graph], feed_dict=feed_dict)[0]
