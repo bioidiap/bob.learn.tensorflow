[layers, shuffler] added datashuffler specific to DR-GAN, fixed the scope in AveragePooling layer

bob/learn/tensorflow/layers/AveragePooling.py
@@ -6,6 +6,9 @@
 import tensorflow as tf
 from .MaxPooling import MaxPooling
 
+import bob.core.log
+import logging
+logger = logging.getLogger("bob.learn")
 
 class AveragePooling(MaxPooling):
 
@@ -40,12 +43,27 @@ class AveragePooling(MaxPooling):
         self.shape = shape
         self.strides = strides
 
+    def create_variables(self, input_layer, scope=None):
+    
+      # get the scope as "network_name/layer_name"
+      if scope is not None:
+        scope = scope + '/' + self.name
+      else:
+        scope = self.name
+
+      self.scope = scope
+
+
+      self.input_layer = input_layer
+      logger.info("== registering input in Average Pooling layer {0}".format(self.name))
+
+
     def get_graph(self, training_phase=True):
         with tf.name_scope(str(self.name)):
             output = tf.nn.avg_pool(self.input_layer, ksize=self.shape, strides=self.strides, padding='SAME')
 
             if self.batch_norm:
-                output = self.batch_normalize(output, training_phase)
+                output = self.batch_normalize(output, training_phase, scope=self.scope)
 
             if self.activation is not None:
                 output = self.activation(output)