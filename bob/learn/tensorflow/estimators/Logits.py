Fix the checkpoint saving when using moving average

bob/learn/tensorflow/estimators/Logits.py
@@ -207,16 +207,15 @@ class Logits(estimator.Estimator):
 
                 train_op = tf.group(
                     self.optimizer.minimize(
-                        self.loss, global_step=global_step),
-                    loss_averages_op)
+                        self.loss, global_step=global_step), loss_averages_op)
 
                 # Get the moving average saver after optimizer.minimize is
                 # called
                 if apply_moving_averages:
-                    self.saver = self.optimizer.swapping_saver()
-                    self.scaffold = tf.train.Scaffold(saver=self.saver)
+                    self.saver, self.scaffold = moving_average_scaffold(
+                        self.optimizer, config)
                 else:
-                    self.scaffold = None
+                    self.saver, self.scaffold = None, None
 
                 # Log accuracy and loss
                 with tf.name_scope('train_metrics'):
@@ -420,3 +419,14 @@ class LogitsCenterLoss(estimator.Estimator):
 
         super(LogitsCenterLoss, self).__init__(
             model_fn=_model_fn, model_dir=model_dir, config=config)
+
+
+def moving_average_scaffold(optimizer, config):
+    max_to_keep = 5 if config is None else config.keep_checkpoint_max
+    keep_checkpoint_every_n_hours = 10000.0 if config is None else \
+        config.keep_checkpoint_every_n_hours
+    saver = optimizer.swapping_saver(
+        max_to_keep=max_to_keep,
+        keep_checkpoint_every_n_hours=keep_checkpoint_every_n_hours)
+    scaffold = tf.train.Scaffold(saver=saver)
+    return saver, scaffold

bob/learn/tensorflow/estimators/Regressor.py
@@ -1,4 +1,5 @@
 from . import check_features, get_trainable_variables
+from .Logits import moving_average_scaffold
 from bob.learn.tensorflow.network.utils import append_logits
 from tensorflow.python.estimator import estimator
 import tensorflow as tf
@@ -120,10 +121,10 @@ class Regressor(estimator.Estimator):
                 # Get the moving average saver after optimizer.minimize is
                 # called
                 if apply_moving_averages:
-                    self.saver = self.optimizer.swapping_saver()
-                    self.scaffold = tf.train.Scaffold(saver=self.saver)
+                    self.saver, self.scaffold = moving_average_scaffold(
+                        self.optimizer, config)
                 else:
-                    self.scaffold = None
+                    self.saver, self.scaffold = None, None
 
                 # Log rmse and loss
                 with tf.name_scope('train_metrics'):

bob/learn/tensorflow/test/test_estimator_onegraph.py
@@ -39,6 +39,8 @@ def test_logitstrainer():
     try:
         embedding_validation = False
         _, run_config, _, _, _ = reproducible.set_seed()
+        run_config = run_config.replace(
+            keep_checkpoint_max=10, save_checkpoints_steps=100, save_checkpoints_secs=None)
         trainer = Logits(
             model_dir=model_dir,
             architecture=dummy,
@@ -223,8 +225,10 @@ def test_moving_average_trainer():
     # train it again with moving average
     # Is it different from no moving average? yes -> good
 
-    no_moving_average = {'accuracy': 0.856, 'loss': 0.55705935, 'global_step': 188}
-    with_moving_average = {'accuracy': 0.052, 'loss': 2.718809, 'global_step': 188}
+    no_moving_average = {'accuracy': 0.856,
+                         'loss': 0.55705935, 'global_step': 188}
+    with_moving_average = {'accuracy': 0.052,
+                           'loss': 2.718809, 'global_step': 188}
 
     try:
         # Creating tf records for mnist
@@ -290,6 +294,34 @@ def test_moving_average_trainer():
         estimator = _estimator(True)
         _evaluate(estimator, with_moving_average)
 
+    finally:
+        try:
+            os.unlink(tfrecord_train)
+            os.unlink(tfrecord_validation)
+            shutil.rmtree(model_dir, ignore_errors=True)
+        except Exception:
+            pass
+
+
+def test_saver_with_moving_average():
+    try:
+        _, run_config, _, _, _ = reproducible.set_seed()
+        run_config = run_config.replace(
+            keep_checkpoint_max=10, save_checkpoints_steps=100,
+            save_checkpoints_secs=None)
+        estimator = Logits(
+            model_dir=model_dir,
+            architecture=dummy,
+            optimizer=tf.train.GradientDescentOptimizer(learning_rate),
+            n_classes=10,
+            loss_op=mean_cross_entropy_loss,
+            embedding_validation=False,
+            validation_batch_size=validation_batch_size,
+            config=run_config)
+        run_logitstrainer_mnist(estimator, augmentation=True)
+        ckpt = tf.train.get_checkpoint_state(estimator.model_dir)
+        assert ckpt, "Failed to get any checkpoint!"
+        assert len(ckpt.all_model_checkpoint_paths) == 10, ckpt.all_model_checkpoint_paths
     finally:
         try:
             os.unlink(tfrecord_train)