dc7eb87ec952495ca39a14fc7352619b56f6c575
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2020-09-08 18:18:37 +0200

    remove more scripts

--- bob/learn/tensorflow/script/keras.py
+++ bob/learn/tensorflow/script/keras.py
@@ -6,12 +6,9 @@ from click_plugins import with_plugins
 
 from bob.extension.scripts.click_helper import AliasedGroup
 
-from .utils import eager_execution_option
-
 
 @with_plugins(pkg_resources.iter_entry_points("bob.learn.tensorflow.keras_cli"))
 @click.group(cls=AliasedGroup)
-@eager_execution_option()
 def keras():
     """Keras-related commands."""
     pass
