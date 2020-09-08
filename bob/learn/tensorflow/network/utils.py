2b86a999b6f84e4b2b297f3a792aae23ea073cb1
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2020-09-08 16:49:48 +0200

    remove the network folder

--- bob/learn/tensorflow/network/utils.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# vim: set fileencoding=utf-8 :
-# @author: Tiago de Freitas Pereira <tiago.pereira@idiap.ch>
-
-# functions were moved
-from ..utils.network import append_logits, is_trainable
